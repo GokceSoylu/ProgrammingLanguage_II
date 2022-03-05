168379b0c654842d9810ddd86f26a02fb5dcaaa9
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-03-05 23:56:07 +0300

    .

---
+++ week4/çalışma soruları/tryQuestion1.c
@@ -0,0 +1,37 @@
+/* m uzunluğunki bir dizide x kez tekrar eden ve en cok tekrar eden  sayıları bulup yzdırınız. m X ve dizi elemanları kullanıcıdsan alınaacaktır*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int x,m,*p,i,j,counter,max=0,max_eleman;
+    printf("dizinizin eleman sayısını giriniz ");
+    scanf("%d",&m);
+    p=malloc(sizeof(int)*m);
+    printf("\ndizinin elemanlarını giriniz lutfen\n");
+    for(i=0;i<m;i++)
+    {
+        printf("%d.eleman",i+1);
+        scanf("%d",&p[i]);
+    }
+    printf("kac kez tekrar eden elemanlrın yazdırılmasını istersiniz ");
+    scanf("%d",&x);
+    
+    for(i=0;i<m;i++)
+    {
+        counter=0;
+        for(j=0;j<m;j++)
+        {
+            if(p[i]==p[j])
+                counter++;
+        }
+        if(counter==x)
+            printf("%d",p[i]);
+        if(counter>max)
+        {
+            max=counter;
+            max_eleman=p[i];
+        }    
+    }
+    printf("en cok tekrar eden eleman = %d",max_eleman);
+    return 0;
+}
