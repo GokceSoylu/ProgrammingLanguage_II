279afb1744bea1a54d3a813b541ba8f1465e70ef
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-03-18 17:41:05 +0300

    .

---
+++ lab5/kelimeSirasi.c
@@ -0,0 +1,23 @@
+/* girilen metini pointer kullanarak son kelimenden ilk kelimeye dogru yazdiriniz */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int size,i,j;
+    char string[200],*p;
+    printf("metni giriniz lutfen");
+    gets(string);
+    for(size=0;string[size]!='\0';size++);
+    size++;
+    p=(char*)malloc(sizeof(char)*size);
+    p=string;
+    printf("\nmetnin kelimelerinin ters sirali hali\n");
+    for(i=size-1;i>=-1;i--)
+        if(*(p+i)==' ' || i==-1)
+        {
+            for(j=i+1;*(p+j)!=' ' && *(p+j)!='\0';j++)
+                printf("%c",*(p+j));
+            printf(" ");
+        }    
+    return 0;
+}
