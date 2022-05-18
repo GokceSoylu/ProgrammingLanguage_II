94a32c0d8df1e489c71e87c7807006589e13114b
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-05-18 17:36:54 +0300

    .

--- week10/fscanf.c
+++ week10/fscanf.c
@@ -9,15 +9,14 @@ int main()
         printf("\nthe file could not be opened ");
     else
     {
-        printf("%s%s%s","account no","name","balace");
+        printf("\naccount no name balace\n");
         fscanf(p,"%d%s%d",&hesapNo,ad,&bakiye);
         while(!feof(p))
         {
-            printf("\n%-10d%-13s%7d",hesapNo,ad,bakiye);
+            printf("\n%d%s%d",hesapNo,ad,bakiye);
             fscanf(p,"%d%s%d",&hesapNo,ad,&bakiye);
-            
         }
         fclose(p);
     }
     return 0;
-}
+} 
