2f651c670640b9e3f0aa83f2c4aee8830f8fb07e
Necmiye Soylu <necmiyesoyla@gmail.com>
2022-05-21 02:49:22 +0300

    .

--- week10/fscanf.c
+++ week10/fscanf.c
@@ -2,22 +2,16 @@
 #include <stdio.h>
 int main()
 {
-    struct student
-    {
-        int hesapNo, bakiye;
-        char ad[20];
-    };
-    typedef struct student std;
-    std a;
-    int counter=1;
+    int hesapNo, bakiye;
+    char ad[20];
     FILE *p;
     p=fopen("musteri","r");
     printf("\naccount no name balace\n");
-    fscanf(p,"%d\n%s\n%d",&a.hesapNo,a.ad,&a.bakiye);
     while(!feof(p))
     { 
-        printf("\n%d\n%s\n%d",a.hesapNo,a.ad,a.bakiye);
-        fscanf(p,"%d\n%s\n%d",&a.hesapNo,a.ad,&a.bakiye);
+        fscanf(p,"%d%s%d",&hesapNo,ad,&bakiye);
+        printf("\n%d\n%s\n%d",hesapNo,ad,bakiye);
+        
     }
     fclose(p);
     return 0;//🤯 bu pointer neden sürekli ilk elemanı yazdırıyor? nedeeeeen?
